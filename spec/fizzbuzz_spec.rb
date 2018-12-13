1a6150bfdee0ec7c50e3aa9835ca47c416f27026
David Mears <kieukmail@gmail.com>
2018-12-13 12:55:46 +0000

    Fizzbuzz implemented as a method on Integer - complete

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,24 @@
+require 'fizzbuzz'
+
+describe 'fizzbuzz' do
+  it 'return fizz when passed 3' do
+    expect(3.fizzbuzz).to eq 'fizz' 
+  end
+  
+  it 'returns buzz when passed 5' do
+    expect(5.fizzbuzz).to eq 'buzz'
+  end
+  
+  it 'returns fizzbuzz when passed 15' do
+    expect(15.fizzbuzz).to eq 'fizzbuzz'
+  end
+  
+  it 'returns 4 when passed 4' do
+    expect(4.fizzbuzz).to eq 4
+  end
+  
+  it 'returns "fizz" for the number 3' do
+    expect(3.fizzbuzz).to eq 'fizz'
+  end
+end
+
